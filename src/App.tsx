085e3035905e50abf95be42cc5a8f3cd1d81e833
Angelo Arcillas <angeloarcillas64@gmail.com>
2024-01-13 18:36:53 +0800

    Init + Setup dependency

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <main className="bg-gray-50 min-h-screen">
+        <section className="max-w-screen-2xl mx-auto">
+          
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
